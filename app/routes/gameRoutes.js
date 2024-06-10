7b9570e69fc3b3e2721239ecd6d7712ba516466f
Youssef Fadili <myoussef.fadili@gmail.com>
2024-06-10 22:05:01 +0200

    tej game.ejs + npm install sqlite3

--- app/routes/gameRoutes.js
+++ app/routes/gameRoutes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 //const gameController = require('../controllers/gameController');
 
 
-router.get('/', (req, res) => {
-    res.render('game'); // Rendre la page game.ejs depuis le dossier views/
-});
+// router.get('/', (req, res) => {
+//     res.render('game'); // Rendre la page game.ejs depuis le dossier views/
+// }); j'ai tej ça
 
 module.exports = router;
