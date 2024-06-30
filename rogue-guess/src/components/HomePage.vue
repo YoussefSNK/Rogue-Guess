44e0a2eefa5d62c17cfdee711f0b018422eb2ca3
Youssef Fadili <myoussef.fadili@gmail.com>
2024-06-30 20:03:33 +0200

    style: la HomePage a de nouveau ses styles

--- rogue-guess/src/components/HomePage.vue
+++ rogue-guess/src/components/HomePage.vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="container">
-    <h1 ref="bienvenue">Bienvenue</h1>
-    <form @submit.prevent="handleSubmit">
-      <label for="name">Nom :</label>
-      <input v-model="name" type="text" id="name" name="name" required placeholder="Choisissez un nom" ref="nameInput">
-      <div class="avatar-container" ref="avatarContainer">
-        <div class="arrow" @click="changeAvatar('prev')">&#10094;</div>
-        <img :src="currentAvatar" alt="Avatar" class="avatar" :class="{'visible': isVisible, 'hidden': !isVisible}">
-        <div class="arrow" @click="changeAvatar('next')">&#10095;</div>
-      </div>
-      <label for="game-code">Code de la partie :</label>
-      <input v-model="gameCode" type="text" id="game-code" name="game-code" placeholder="Laissez vide pour créer une partie">
-      <button type="submit" class="button">Jouer</button>
-    </form>
-    <a href="/formulaire" class="btn">Une suggestion ?</a>
+  <div class="home">
+    <div class="background-ball" :style="{ backgroundColor: currentAvatar.firstColor }"></div>
+    <div class="container">
+      <h1 ref="bienvenue">Bienvenue</h1>
+      <form @submit.prevent="handleSubmit">
+        <!-- <label for="name">Nom :</label> -->
+        <input v-model="name" type="text" id="name" name="name" required placeholder="Choisissez un nom" ref="nameInput">
+        <div class="avatar-container" ref="avatarContainer">
+          <div class="arrow" @click="changeAvatar('prev')" :style="{ color: currentAvatar.secondColor }">&#10094;</div>
+          <img :src="currentAvatar.src" alt="Avatar" class="avatar" :class="{'visible': isVisible, 'hidden': !isVisible}">
+          <div class="arrow" @click="changeAvatar('next')" :style="{ color: currentAvatar.secondColor }">&#10095;</div>
+        </div>
+        <label for="game-code">Code de la partie :</label>
+        <input v-model="gameCode" type="text" id="game-code" name="game-code" placeholder="Laissez vide pour créer une partie">
+        <button type="submit" class="button">Jouer</button>
+      </form>
+      <a href="/formulaire" class="btn">Une suggestion ?</a>
+    </div>
   </div>
 </template>
 
 <script>
-import '@/assets/css/indexejs.css';
+// import '@/assets/css/indexejs.css';
 
 import avatar1 from '@/assets/images/avatar/avatar1.png';
 import avatar2 from '@/assets/images/avatar/avatar2.png';
@@ -38,22 +41,27 @@ export default {
       gameCode: '',
       currentIndex: 0,
       avatars: [
-        avatar1,
-        avatar2,
-        avatar3,
-        avatar4,
-        avatar5,
-        avatar6,
-        avatar7,
-        avatar8,
-        avatar9,
+        { src: avatar1, firstColor: '#f0f0f0' }, // secondColor: '#0f0f0f', thirdColor: '#a44ca4' },
+        { src: avatar2, firstColor: '#00a2e8' },
+        { src: avatar3, firstColor: '#4f4f4f' },
+        { src: avatar4, firstColor: '#daadda' },
+        { src: avatar5, firstColor: '#4f060a' },
+        { src: avatar6, firstColor: '#b4e61d' },
+        { src: avatar7, firstColor: '#fff200' },
+        { src: avatar8, firstColor: '#7092be' },
+        { src: avatar9, firstColor: '#ff7f27', secondColor: '#6c2c6c', thirdColor: '#a44ca4' }
       ],
       isVisible: true,
     };
   },
   computed: {
     currentAvatar() {
-      return this.avatars[this.currentIndex];
+      return {
+        src: this.avatars[this.currentIndex].src,
+        firstColor: this.avatars[this.currentIndex].firstColor,
+        secondColor: this.avatars[this.currentIndex].secondColor,
+        thirdColor: this.avatars[this.currentIndex].thirdColor
+      };
     },
   },
   methods: {
@@ -65,7 +73,7 @@ export default {
       }
     },
     createGame() {
-      const userInfo = { username: this.name, avatar: this.currentAvatar, uuid: this.uuidv4(), state: 'alive' };
+      const userInfo = { username: this.name, avatar: this.currentAvatar.src, uuid: this.uuidv4(), state: 'alive' };
       this.$socket.send(JSON.stringify({ type: 'create_game', userInfo }));
     },
     changeAvatar(direction) {
@@ -99,18 +107,153 @@ export default {
     this.addFloatAnimation(this.$refs.avatarContainer);
     this.addFloatAnimation(this.$refs.nameInput);
     this.addFloatAnimation(this.$refs.bienvenue);
-    // Ajouter l'écouteur pour les messages WebSocket
     this.$socket.addEventListener('message', event => {
       const data = JSON.parse(event.data);
       if (data.type === 'game_created' || data.type === 'game_joined') {
-        // Rediriger vers la vue Chat avec le code de partie
         const gameCode = encodeURIComponent(data.gameCode);
         this.$router.push({ name: 'Room', params: { gameCode } });
-      } else if (data.type === 'error') {
-        // Afficher une alerte en cas d'erreur
-        alert(data.message);
       }
     });
+    // this.$socket.addEventListener('open', event => {console.log('Connecté au serveur depuis index.ejs');});
+    // this.$socket.addEventListener('close', event => {console.log('Déconnecté d\'index.ejs');});
+    // this.$socket.addEventListener('error', function(event) {console.error('Error occurred:', event);});
   }
 };
 </script>
+
+<style scoped>
+.home {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #000;
+  color: #fff;
+}
+
+.container {
+  text-align: center;
+  animation: float 10s ease-in-out infinite;
+}
+
+h1 {
+  margin-bottom: 20px;
+  color: #fff; /* Changer la couleur du texte en blanc */
+}
+
+label {
+  display: block;
+  margin-bottom: 10px;
+  color: #fff; /* Changer la couleur du texte en blanc */
+}
+
+input[type="text"] {
+  padding: 8px;
+  font-size: 16px;
+  width: 250px;
+  margin-bottom: 20px;
+  background-color: #333; /* Fond des champs de texte en gris foncé */
+  color: #fff; /* Texte des champs de texte en blanc */
+  border: 1px solid #555; /* Bordure des champs de texte en gris foncé */
+  border-radius: 5px;
+}
+
+.avatar-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.avatar {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  border: 2px solid transparent;
+  transition: border-color 0.3s ease, opacity 0.5s ease, transform 0.5s ease;
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.avatar.visible {
+  opacity: 1;
+  transform: scale(1);
+}
+
+.avatar.hidden {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.arrow {
+  font-size: 2rem;
+  cursor: pointer;
+  user-select: none;
+  padding: 0 10px;
+  transition: color 0.3s ease;
+  color: #fff; /* Changer la couleur des flèches en blanc */
+}
+
+.arrow:hover {
+  color: #007BFF; /* Changer la couleur au survol */
+}
+
+.button {
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.button:hover {
+  background-color: #0056b3;
+}
+
+@keyframes float {
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-10px);
+  }
+}
+
+.floating-element {
+  animation: float 3s ease-in-out infinite;
+}
+
+.background-ball {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 500px; /* Taille initiale de la balle */
+  height: 500px; /* Taille initiale de la balle */
+  border-radius: 50%;
+  animation: color-change 60s linear infinite, size-change 5s ease-in-out infinite;
+  transform: translate(-50%, -50%);
+  /* z-index: -1; */
+  filter: blur(50px); /* Ajout du flou */
+}
+
+@keyframes size-change {
+  0% {
+    width: 650px;
+    height: 650px;
+  }
+  90% {
+    width: 5px;
+    height: 5px;
+  }
+  100%{
+    width: 650px;
+    height: 650px;
+  }
+}
+</style>
